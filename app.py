a0e1cd14b8de65d1798813b76b7f67481dffcae3
cuellobryant-byte <cuellobryant@gmail.com>
2026-04-26 22:35:06 -0400

    Add error handling for invalid input and division by zero

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 print("Welcome to my Savings Goal Tracker!")
 goal = input("What is your savings goal amount?")
 weekly_savings = input("How much do you have to save per week?")
-goal = float(goal)
+try: goal = float(goal)
 weekly_savings = float(weekly_savings)
+if weekly savings == 0: print("weekly savings cannot be zero.")exit()
+except Value Error:print(please enter valid numbers.") exit()
 weeks_needed = goal / weekly_savings
 print(f"It will take approximately {weeks_needed:.1f" weeks to reac your savings goal.")
