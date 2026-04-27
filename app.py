774764608b6caae1c97c26efbcaf26f67a930b6c
cuellobryant-byte <cuellobryant@gmail.com>
2026-04-26 22:30:38 -0400

    Add calculation to estimate weeks needed to reach goal

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
 print("Welcome to my Savings Goal Tracker!")
 goal = input("What is your savings goal amount?")
 weekly_savings = input("How much do you have to save per week?")
+goal = float(goal)
+weekly_savings = float(weekly_savings)
+weeks_needed = goal / weekly_savings
