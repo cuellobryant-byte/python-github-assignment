b573786987327f67d36b4aa915049dc21808f3ad
cuellobryant-byte <cuellobryant@gmail.com>
2026-04-26 22:32:14 -0400

    Add formatted output displaying time to reach savings goal

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ weekly_savings = input("How much do you have to save per week?")
 goal = float(goal)
 weekly_savings = float(weekly_savings)
 weeks_needed = goal / weekly_savings
+print(f"It will take approximately {weeks_needed:.1f" weeks to reac your savings goal.")
