351b69cbb140ca35eaa26a387819342dfa594aec
cuellobryant-byte <cuellobryant@gmail.com>
2026-04-26 22:40:12 -0400

    Clean up code and finalize program

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 print("Welcome to my Savings Goal Tracker!")
-goal = input("What is your savings goal amount?")
-weekly_savings = input("How much do you have to save per week?")
-try: goal = float(goal)
-weekly_savings = float(weekly_savings)
-if weekly savings == 0: print("weekly savings cannot be zero.")exit()
-except Value Error:print(please enter valid numbers.") exit()
+goal = input("What is your savings goal amount? ")
+weekly_savings = input("How much do you save per week? ")
+try:
+    goal = float(goal)
+    weekly_savings = float(weekly_savings)
+    if weekly_savings == 0:
+        print("Weekly savings cannot be zero.")
+        exit()
+except ValueError:
+    print("Please enter valid numbers.")
+    exit()
+
 weeks_needed = goal / weekly_savings
-print(f"It will take approximately {weeks_needed:.1f" weeks to reac your savings goal.")
+print(f"It will take approximately {weeks_needed:.1f} weeks to reach your savings goal.")
